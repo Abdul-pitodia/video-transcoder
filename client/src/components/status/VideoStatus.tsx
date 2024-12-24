a10251003fc867ee1fe2824a6cebabadbca37839
Abdul Pitodia <abdulpitodia9@gmail.com>
2024-12-24 20:13:18 +0530

    Fix

--- client/src/components/status/VideoStatus.tsx
+++ client/src/components/status/VideoStatus.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import VideoHLSPLayer from "../live-stream/VideoHLSPlayer";
 import { VideoStatusProps } from "./VideoStatusList";
 
